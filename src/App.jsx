6de3b9991a39c4634d963b0edac71383b2e0c408
veeravelS <sveeravel16@gmail.com>
2025-03-02 11:44:22 +0530

    changes in app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router,Routes,Route, Navigate} from 'react-router-dom'
 import './App.css'
-<<<<<<< HEAD
 import Login from './pages/auth/Login'
-=======
->>>>>>> e04b00b9bdca7210c6487b283f487354fd5f185b
 import PageLayout from './pages/layout/PageLayout'
 import Project from './pages/layout/project/Project'
 import Task from './pages/layout/task/Task'
@@ -13,15 +10,12 @@ import TaskStatus from './pages/layout/Task status/TaskStatus'
 import Roles from './pages/layout/roles/Roles'
 import CompanySetting from './pages/layout/company setting/CompanySetting'
 import ProtectedRoute from './ProtectedRoute'
-<<<<<<< HEAD
 
-=======
 import { ConfigProvider } from 'antd'
 import { themeConfig } from './config/theme'
 import Message from './pages/layout/message/Message'
 import NoPage from './pages/NoPage'
 import Login from './pages/auth/Login'
->>>>>>> e04b00b9bdca7210c6487b283f487354fd5f185b
 
 
 function App() {
@@ -33,14 +27,9 @@ function App() {
         <Route  path='/' element={<Navigate to="/login" replace />} />
         <Route path='/login' element={<Login />} />
          <Route element={<ProtectedRoute />}>
-<<<<<<< HEAD
-        <Route path='/'  element={<PageLayout />}>
-          <Route  path='/project' element={<Project />} />
-=======
         <Route  element={<PageLayout />}>
           <Route path='/'  element={<Navigate to="/project" />} />
           <Route path='/project' element={<Project />} />
->>>>>>> e04b00b9bdca7210c6487b283f487354fd5f185b
           <Route path='/task' element={<Task />} />
           <Route path='/user' element={<User />} />
           <Route path='/attendance-report' element={<AttendanceReport />} />
@@ -50,12 +39,8 @@ function App() {
           <Route path='/task-status' element={<TaskStatus />} />
           <Route path='/company-setting' element={<CompanySetting />} />
         </Route>
-<<<<<<< HEAD
       </Route>
-=======
           <Route path='*' element={<NoPage />} />
-       </Route> 
->>>>>>> e04b00b9bdca7210c6487b283f487354fd5f185b
       </Routes>
     </Router>
     </ConfigProvider>
